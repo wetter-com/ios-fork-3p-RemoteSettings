4bd393d28858bcbdbf69d9d662029e4bd27045ae
Pascal Stüdlein <stuedlein@tbointeractive.com>
2017-10-16 10:11:49 +0200

    Added empty Xcode project

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RemoteSettings' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  target 'RemoteSettingsTests' do
+    pod 'Quick', '~> 1.1'
+    pod 'Nimble', '~> 7.0.2'
+  end
+
+end
---
+++ RemoteSettings/RemoteSettings.h
@@ -0,0 +1,19 @@
+//
+//  RemoteSettings.h
+//  RemoteSettings
+//
+//  Created by Pascal Stüdlein on 16.10.17.
+//  Copyright © 2017 TBO interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RemoteSettings.
+FOUNDATION_EXPORT double RemoteSettingsVersionNumber;
+
+//! Project version string for RemoteSettings.
+FOUNDATION_EXPORT const unsigned char RemoteSettingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteSettings/PublicHeader.h>
+
+
